feeac5f32b79b3773321f17791a2006f3f06b3c7
Marcio Vale <marciovale04@gmail.com>
2026-01-11 22:22:32 +0000

    Corrigir config Postgres para Render

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,14 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "123", // a mesma do pgAdmin
-  database: "postgres",
-  port: 5432
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
